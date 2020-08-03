3f49cecfe2e763677e687fe764d3e02762d7aa39
zhangchugao <1259563100@qq.com>
2020-08-03 19:26:09 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
-/*
- * @Author: 极客James
- * @Motto: 求知若渴,虚心若愚
- * @Github: https://github.com/Geek-James/ddBuy
- * @掘金: https://juejin.im/user/5c4ebc72e51d4511dc7306ce
- * @LastEditTime: 2019-11-07 17:32:25
- * @Description: 项目主入口
- * @FilePath: /ddBuy/src/main.js
- */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
